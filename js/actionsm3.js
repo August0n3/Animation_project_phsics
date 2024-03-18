d55c0f90f7608fbf9f1492317ecb88cf0298816e
August0n3 <august0n3211@gmail.com>
2024-03-18 17:37:22 -0300

    Arrumando parte inicial da animação da q3

--- js/actionsm3.js
+++ js/actionsm3.js
@@ -9,46 +9,35 @@ let col = false
 
 function play(){
     let ob1 = document.getElementById('obj1')
-    let ob2 = document.getElementById('obj2')
-    let posy = 0
+    let posy = -10
     let posx = 10
+    let dist = 0
     ob1.style.left = posx +'px'
     ob1.style.top = posy +'px';
     clearInterval(id2)
     clearInterval(id)
-    id2 = setInterval(verificarColisao, 10)
     id = setInterval(framey, 10)
-   
-    
+  
     function framey(){ 
         if( posy >= 480){
             clearInterval(id)
             clearInterval(id2)
             tempo = 0
         }else{
-            if (col == true){
-            posx+=1
+            let dist = Math.sqrt( ((300-posx)*(300-posx))  + ((150-posy)*(150-posy)))
+            console.log(dist)
+            if (dist > 320 && posy>100){
+                if (posx > 50){
+                    posx+=2
+                }
+                else{
+                    posx+=1
+                }
             ob1.style.left = posx +'px';
-        }else{
-            posy+= 1
+            }else{
+            posy+=4
             ob1.style.top = posy +'px';
-            
-        }
-    }
-    }
-    function verificarColisao() {
-        const div1Rect = ob1.getBoundingClientRect();
-        const div2Rect = ob2.getBoundingClientRect();
-    
-        if (
-            div1Rect.right >= div2Rect.left &&
-            div1Rect.left <= div2Rect.right &&
-            div1Rect.bottom >= div2Rect.top &&
-            div1Rect.top <= div2Rect.bottom
-        ) {
-            col = true;
-        } else {
-            col = false;
+            }
         }
     }
 }   
