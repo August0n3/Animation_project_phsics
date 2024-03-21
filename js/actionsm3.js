a4bb1f8a1a10154d7f4dcbb73a0ed47ddb243a8d
August0n3 <august0n3211@gmail.com>
2024-03-21 12:15:14 -0300

    att qst 3

--- js/actionsm3.js
+++ js/actionsm3.js
@@ -1,43 +1,54 @@
-let r = 5 //raio do circulo
-let h = (5*r)/2
-let id = null
-let id2 = null
-let g = 2
-let col = false
+let r = 5; // Raio do círculo
+let h = (5 * r) / 2;
+let id = null;
+let g = 2;
 
+function play() {
+  let circle = false;
+  let ob1 = document.getElementById("obj1");
+  let posy = -10;
+  let posx = 10;
+  let velocidade = 0;
+  let dist = 0;
+  ob1.style.left = posx + "px";
+  ob1.style.top = posy + "px";
+  clearInterval(id);
 
+  id = setInterval(framey, 10);
 
-function play(){
-    let ob1 = document.getElementById('obj1')
-    let posy = -10
-    let posx = 10
-    let dist = 0
-    ob1.style.left = posx +'px'
-    ob1.style.top = posy +'px';
-    clearInterval(id2)
-    clearInterval(id)
-    id = setInterval(framey, 10)
-  
-    function framey(){ 
-        if( posy >= 480){
-            clearInterval(id)
-            clearInterval(id2)
-            tempo = 0
-        }else{
-            let dist = Math.sqrt( ((300-posx)*(300-posx))  + ((150-posy)*(150-posy)))
-            console.log(dist)
-            if (dist > 320 && posy>100){
-                if (posx > 50){
-                    posx+=2
+  function framey() {
+    console.log(Math.ceil(posx));
+    console.log(posy)
+    if (posx >= 680) {
+      clearInterval(id);
+    } else {
+        
+        if (posx<350 && circle == false) {
+            dist = Math.sqrt(((300 - posx) * (300 - posx)) + ((150 - posy) * (150 - posy)));
+            if (dist > 320 && posy > 100) {
+                let angulo = Math.atan2(posy - 150, posx - 300);
+                if (posx > 50) {
+                posx += Math.sin(angulo) * 2;
+                } else {
+                posx += Math.sin(angulo)*3;
                 }
-                else{
-                    posx+=1
-                }
-            ob1.style.left = posx +'px';
-            }else{
-            posy+=4
-            ob1.style.top = posy +'px';
+            } else {
+                posy +=3;
             }
         }
+        else{
+            circle = true;
+            dist = Math.sqrt(((350 - posx) * (350 - posx)) + ((430 - posy) * (430 - posy)));
+            //console.log(dist);
+            let angulo = Math.atan2(posy - 430, posx - 350);
+            if (dist != 50) {
+                posy -=  Math.cos((angulo));
+                posx +=  Math.sin((angulo));  
+            } 
+        }
+
     }
-}   
+    ob1.style.left = posx + "px";
+    ob1.style.top = posy + "px";
+  }
+}
